0d35faac8aea09b9e4969c4f8418a9927bd04492
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-08-08 02:22:28 -0500

    deriving state from props

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react";
 import PlayerInfo from "./components/PlayerInfo";
 import GameBoard from "./components/GameBoard";
+import Log from "./components/Log";
 
 function App() {
+  const [gameTurns, setGameTurns] = useState([]);
   const [activePlayer, setActivePlayer] = useState('X'); 
 
-  function handleSelectSquare(){
+  function handleSelectSquare(rowIndex, colIndex){
     setActivePlayer((currentActivePlayer) => currentActivePlayer === 'X' ? 'O': 'X'); 
+    setGameTurns(prevTurns => {
+      let currentPlayer = 'X'; 
+
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O'; 
+      }
+
+      const updatedTurns = 
+      [{square: {row: rowIndex, col: colIndex}, player:currentPlayer}, ...prevTurns,];
+
+      return updatedTurns; 
+
+    } ); 
   }
 
   return (
@@ -16,7 +31,9 @@ function App() {
          <PlayerInfo initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} /> 
          <PlayerInfo initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} /> 
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} activePlayerSymbol={activePlayer} />
+        <GameBoard 
+          onSelectSquare={handleSelectSquare} 
+          turns={gameTurns} />
       </div>
 
       LOG
