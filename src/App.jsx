b9562937f7093e7cb9091efdc52a40a75c78464d
Jack Cahit Karakas <ckarakassdet@gmail.com>
2024-08-09 12:38:46 -0500

    finished the game

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,10 @@ function deriveActivePlayer(gameTurns){
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1', 
+    O: 'Player 2',
+  }); 
   const [gameTurns, setGameTurns] = useState([]);
   // const [activePlayer, setActivePlayer] = useState('X'); 
 
@@ -49,7 +53,7 @@ function App() {
       firstSquareSymbol === thirdSquareSymbol
     )   
     {
-      winner = firstSquareSymbol; 
+      winner = players[firstSquareSymbol]; 
     } 
   }
 
@@ -70,12 +74,21 @@ function App() {
     setGameTurns([]); 
   }
 
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    }); 
+  }
+
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <PlayerInfo initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
-          <PlayerInfo initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
+          <PlayerInfo initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} onChangeName={handlePlayerNameChange}/>
+          <PlayerInfo initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} onChangeName={handlePlayerNameChange}/>
         </ol>
         {(winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart}/>}
         <GameBoard
--- src/components/PlayerInfo.jsx
+++ src/components/PlayerInfo.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
-export default function PlayerInfo({ initialName, symbol, isActive}) {
+export default function PlayerInfo({ initialName, symbol, isActive, onChangeName}) {
     const [playerName, setPlayerName] = useState(initialName); 
     const [isEditing, setIsEditing] = useState(false);
 
     function handleEditClick() {
         setIsEditing((editing) => !editing);
+        if(isEditing){
+            onChangeName(symbol, playerName); 
+        }  
     }
 
     //let btnCaption = 'Edit'; 
